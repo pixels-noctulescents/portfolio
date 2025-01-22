8a38b32fba57a12dd599bbd30121f8d216563fd2
Adam VO <adam.vo@ubiquid.fr>
2025-01-23 00:06:39 +0100

    Update style

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "@fontsource/ibm-plex-mono/400.css";
 import "@fontsource/ibm-plex-mono/500.css";
 import "@fontsource/ibm-plex-mono/600.css";
 import "@fontsource/ibm-plex-mono/700.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
