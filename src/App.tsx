96bfd7759737e10f125a4057ae992c35351f27b3
Adam VO <adam.vo@ubiquid.fr>
2025-02-11 22:23:37 +0100

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import { FC } from "react";
 import styles from "./App.module.css";
-import bg from "./bg.jpg";
+import img from "./background.svg";
 
 const App: FC = () => {
+  console.log(img);
   return (
     <div className={styles.wrapper}>
-      <div
-        className={styles.background}
-        style={{ backgroundImage: `url(${bg})` }}
-      ></div>
-      <div className={styles.border}></div>
       <h1>Adam Phi Vô</h1>
-      <h2>web developer</h2>
-      <h3>coming soon</h3>
+      <h3>©2025</h3>
     </div>
   );
 };
